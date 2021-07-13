a463b230cb8b1d7aac67d2494c7bd77c2cce19c3
Thanh Minh Le <75014161+itstarsec@users.noreply.github.com>
2021-07-13 11:56:55 +0700

    Create python-pingsweep.py
    2 – Try to do the above exercise with a higher-level scripting language such as Python, Perl, or Ruby

---
+++ Mod_02_04/python-pingsweep.py
@@ -0,0 +1,11 @@
+import os
+
+def main():
+  network = str(input("Please input range network: "))
+  for i in range(1,254,1):
+    cmd = "ping -c 1 " str(network) + "." + str(i) + """ | grep "64 bytes" | cut -d " " -f 4 | sed 's\.$\\' &"""
+    i += 1
+    os.system(cmd)
+    
+if __name__ == "__main__":
+  main()
