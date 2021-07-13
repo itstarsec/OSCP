e01f1d3c72de6782fc089e5848d14e11c3151262
Thanh Minh Le <75014161+itstarsec@users.noreply.github.com>
2021-07-13 11:53:01 +0700

    Create pingsweep.sh
    1 – Research Bash loops and write a short script to perform a ping sweep of your target IP range of 10.11.1.0/24.

---
+++ Mod_02_04/pingsweep.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [ "$1" == "" ]
+then
+echo "Usage: ./pingsweep.sh [network]"
+echo "Example: ./pingsweep.sh 10.11.1"
+else
+for ip in `seq 1 254`; do
+ping -c $1.$ip | grep "64 bytes" | cut -d " " -f 4 | sed 's\.$\\' $
+done
+fi
