f78a63a8da42f8b642ec65f9e362cb1b62775a74
Thanh Minh Le <75014161+itstarsec@users.noreply.github.com>
2021-11-16 14:51:34 +0700

    Add files via upload

---
+++ Active Directory Attacks/enumerateAD.ps1
@@ -0,0 +1,69 @@
+﻿$domainObj = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain()
+
+$PDC = ($domainObj.PdcRoleOwner).Name
+
+$SearchString = "LDAP://"
+
+$SearchString += $PDC + "/"
+
+$DistinguishedName = "DC=$($domainObj.Name.Replace('.', ',DC='))"
+
+$SearchString +=$DistinguishedName
+
+$SearchString
+
+$Searcher = New-Object System.DirectoryServices.DirectorySearcher([ADSI]$SearchString)
+
+$objDomain = New-Object System.DirectoryServices.DirectoryEntry
+
+$Searcher.SearchRoot = $objDomain
+
+$Searcher.filter="(name=IT Admins)"
+
+$Result = $Searcher.FindAll()
+
+Foreach($obj in $Result)
+{
+	$obj.Properties.member
+}
+
+
+
+
+
+
+
+
+
+#---------
+#$Searcher.filter="samAccountType=805306368"
+
+
+#$Result = $Searcher.FindAll()
+
+#Foreach($obj in $Result)
+#{
+#	Foreach($prop in $obj.Properties)
+#	{
+#		$prop
+#	}
+#	
+#	Write-Host "-------------------------------"
+#}
+
+
+
+#$Searcher.filter="name=Administrator"
+#
+#$Result = $Searcher.FindAll()
+#
+#Foreach($obj in $Result)
+#{
+#	Foreach($prop in $obj.Properties)
+#	{
+#		$prop
+#	}
+#	
+#	Write-Host "-------------------------------"
+#}
+
