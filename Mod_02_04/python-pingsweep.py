560c7ba43637d03f61c0489a4dc346433e1822cc
Thanh Minh Le <75014161+itstarsec@users.noreply.github.com>
2023-01-17 11:46:43 +0700

    Update python-pingsweep.py

--- Mod_02_04/python-pingsweep.py
+++ Mod_02_04/python-pingsweep.py
@@ -3,7 +3,7 @@ import os
 def main():
   network = str(input("Please input range network: "))
   for i in range(1,254,1):
-    cmd = "ping -c 1 " str(network) + "." + str(i) + """ | grep "64 bytes" | cut -d " " -f 4 | sed 's\.$\\' &"""
+    cmd = "ping -c 1 " str(network) + "." + str(i) + """ | grep "64 bytes" | cut -d " " -f 4 | sed 's/.$//g' &"""
     i += 1
     os.system(cmd)
     
